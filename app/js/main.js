778f019d6732feefdc9543630d279c3c56bfe43b
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-04-17 01:10:48 +0300

    Header arrows 17 апреля

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,9 @@ $('.header__slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   fade: true,
-  cssEase: 'linear'
+  cssEase: 'linear',
+  prevArrow: `<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="link arrow-left">`,
+  nextArrow: `<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="link arrow-right">`
 
 })
 
