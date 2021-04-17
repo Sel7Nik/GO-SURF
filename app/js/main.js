5d19e9debc768d4030f8927983ad630f8330ead9
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-04-17 11:38:04 +0300

    Header slider-dots

--- app/js/main.js
+++ app/js/main.js
@@ -7,9 +7,19 @@ $('.header__slider').slick({
   fade: true,
   cssEase: 'linear',
   prevArrow: `<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="link arrow-left">`,
-  nextArrow: `<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="link arrow-right">`
-
+  nextArrow: `<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="link arrow-right">`,
+  asNavFor: '.slider-dots'
 })
 
-  
+$('.slider-dots').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  asNavFor: '.slider-for',
+  dots: true,
+  focusOnSelect: true,
+  asNavFor: '.header__slider'
+});
+
+
+
 });
